15e187707de789424db148effc3dd2733f6084d9
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-01-22 10:57:58 +0500

    if else revision

---
+++ Javascript/class 4/app.js
@@ -0,0 +1,151 @@
+// var productOne = prompt("Enter the product one name")
+// var productOnePrice = parseInt(prompt("Enter product one price"));
+// var productTwo = prompt("Enter the product two name");
+// var productTwoPrice = parseInt(prompt("Enter the product two price"));
+// var productThree = prompt("Enter the product three name");
+// var productThreePrice = parseInt(prompt("Enter the product three price"));
+// var productFour = prompt("Enter the product four name");
+// var productFourPrice = parseInt(prompt("Enter the product three price"));
+// var productFive = prompt("Enter the product five name");
+// var productFivePrice = parseInt(prompt("Enter the product five price"));
+
+// var productTotal = productOnePrice + productTwoPrice + productThreePrice + productFourPrice + productFivePrice;
+
+// var discount = 0;
+
+
+
+
+// if (productTotal >= 10000) {
+//     discount = (productTotal * 0.1)
+// }
+// else if (productTotal >= 5000) {
+//     discount = (productTotal * 0.05)
+// }
+// else if (productTotal >= 3000) {
+//     discount = (productTotal * 0.03)
+// }
+// else { alert(`${discount} discount less then 3000`) };
+
+
+// document.write(`
+//      <table border=''>
+//         <tr>
+//             <th>Product Name</th>
+//             <th>Product Price</th>
+//         </tr>
+//         <tr>
+//             <td>${productOne}</td>
+//             <td>${productOnePrice}</td>
+//         </tr>
+//         <tr>
+//             <td>${productTwo}</td>
+//             <td>${productTwoPrice}</td>
+//         </tr>
+//         <tr>
+//             <td>${productThree}</td>
+//             <td>${productThreePrice}</td>
+//         </tr>
+//         <tr>
+//             <td>${productFour}</td>
+//             <td>${productFourPrice}</td>
+//         </tr>
+//         <tr>
+//             <td>${productFive}</td>
+//             <td>${productFivePrice}</td>
+//         </tr>
+//         <tr>
+//             <th>Total Amount</th>
+//             <td>${productTotal}</td>
+//         </tr>
+//         <tr>
+//             <th>Discount</th>
+//             <td>${discount}</td>
+//         </tr>
+//         <tr>
+//             <th>Amount Payble</th>
+//             <td>${productTotal - discount}</td>
+//         </tr>
+//     </table> 
+//     `)
+
+
+
+// var pass = "admin"
+// var userPass = prompt("Enter your password")
+
+// if(userPass == null || userPass == ''){
+//     alert("enter any value please")
+// }
+// else if(userPass == pass){
+//     alert("Your password is correct")
+// }
+// else{
+//     alert("your password is wrong")
+// }
+
+// guess the number
+
+// var myNum = 5
+// var userInp = prompt("Enter your socha hua number")
+
+// if(userInp == myNum){
+//     document.write(`my value was ${myNum} & your value was ${userInp} thatswhy you are correct`)
+// }
+// else if(userInp == 6){
+//     document.write(`my value was ${myNum} & your value was ${userInp} thatswhy you are kareeb kareeb`)
+// }
+// else if(userInp == 4){
+//     document.write(`my value was ${myNum} & your value was ${userInp} thatswhy you are kareeb kareeb`)
+// }
+// else{
+//     document.write(`my value was ${myNum} & your value was ${userInp} thatswhy you are mudassir`)
+// }
+
+
+// var username = 'admin'
+// var password = '12345'
+// var userinpName = prompt("enter your username")
+// var userinpPass = prompt("enter your userpass")
+
+
+// if (userinpName == username && userinpPass == password) {
+//     alert("youre welcome")
+// }
+// else {
+//     alert("Nikal bhai")
+// }
+
+
+
+// var userInp = +prompt("Enter any number")
+// if (userInp % 3 == 0) {
+//     alert("number is divisible by 3")
+// }
+// else {
+//     alert('not divisible by 3')
+// }
+
+// var c = 12
+
+// if(c++ == 13){
+//     document.write("con 1 is true")
+// }
+// if(c === 13){
+//     document.write("con 2 is true") // this is true
+// }
+// if(++c < 14){
+//     document.write("con 3 is true")
+// }
+// if(c === 14){
+//     document.write(" con 4 is true")
+// }
+
+
+
+
+
+
+
+
+
