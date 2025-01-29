4074008e89e55bb333d939ceaf5734d9fe521d44
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-01-29 10:53:52 +0500

    array and 6 methods

---
+++ Javascript/class 5/app.js
@@ -0,0 +1,46 @@
+// var student = ['Taha', 'Rehman', 'Talha', 'Anjum']
+// document.write(student[0])
+// document.write(student[4])
+
+// var mixArray = ['fahad', 30, true, NaN, 19.65]
+// document.write(mixArray)
+
+// var sabzi = ['tindey', 'kheera', 'loki', 'gajar', 'moli']
+// sabzi.splice(1,4,'banana')
+// document.write(sabzi)
+
+// splice(starting index, how many delete, add elements)
+// sabzi.splice(3,1)
+
+// sabzi.unshift("mango","apple")
+// sabzi.shift()
+// sabzi.shift()
+// sabzi.shift()
+// sabzi.shift()
+// sabzi.shift()
+
+
+// sabzi.push('dhaniya','podena')
+// sabzi.pop()
+// sabzi.pop()
+// sabzi.pop()
+
+
+// push (last se add kar raha tha)
+// pop  (last se delete kar raha tha)
+// shift (shru se delete kar raha tha)
+// unshift (shru se add kar raha tha )
+// slice ( copy karega or new variable ko dedega )
+// splice (kahin se bhi add, kahin se bhi delete kar raha tha)
+
+// var fruits = ['apple', 'banana', 'kiwi', 'mango', 'blueberry']
+// var copiedArray = fruits.slice(2,3)
+// document.write(copiedArray)
+
+// var friends = ['mudassir', 'yasir', 'ehtisham', 'ali', 'anas', 'affan']
+// var copiedArray = friends.slice(1,5)
+// document.write(copiedArray)
+
+
+
+
