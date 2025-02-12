4740155ca5bc8d52eef5b95ee731f78d4ddbf45b
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-02-12 10:39:33 +0500

    string methods

---
+++ Javascript/class 11/app.js
@@ -0,0 +1,61 @@
+// var str = 'HELLO world'
+// document.write(str.toLowerCase())
+
+// var str = 'saylani'
+// var copyFirst = str[0].toUpperCase()
+// document.write(copyFirst)
+
+// var user = prompt("Enter any word")
+// var copyFirst = user.slice(0,1).toUpperCase()
+// var copyAll = user.slice(1).toLowerCase()
+// var conc = copyFirst + copyAll
+// document.write(conc)
+
+// var cars = ['Mehran', 'Fx', 'Civic', 'Alto']
+// var user = prompt("Enter any car") // mehran, MehRan, MEHRAN
+// var copyFirst = user.slice(0,1).toUpperCase()
+// var copyAll = user.slice(1).toLowerCase()
+// var conc = copyFirst + copyAll
+// var check = 'no'
+// for(var i=0; i < cars.length; i++){
+//     if(conc == cars[i]){
+//         check = 'yes'
+//         document.write(conc + " car available")
+//     }
+// }
+// if(check == 'no'){
+//     document.write(conc + " car not available")
+// }
+
+// var str = 'saylani smit hello world'
+// var name1 = 'pappu'
+// var eq = str.split(' ').reverse().concat(name1).join(' ')
+// console.log(eq)
+
+// var str = 'yasir'
+// var eq = str.split('').reverse().join('')
+// document.write(eq)
+
+// var cars = ['Mehran', 'Fx', 'Civic', 'Alto', 'Mehran']
+// var eq = cars.lastIndexOf('Mehran')
+// document.write(eq)
+
+// var str = 'yasir'
+// document.write(str.charCodeAt(2))
+
+// var str = 'hello hello'
+// var rep = str.replaceAll('hello','bhai')
+// document.write(rep)
+
+// var mobile = 'hyderabad'
+
+// document.write(mobile.replace('hyder','Islam'))
+
+
+var num = '420'
+
+document.write(typeof(Number(num)))
+
+
+
+
