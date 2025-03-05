6d5a5970499ae45c4ed922d28361b18c11c828ed
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2025-03-05 10:23:12 +0500

    onclick and id

---
+++ Javascript/class 18/app.js
@@ -0,0 +1,32 @@
+// function foo(){
+//     alert("hello world")
+// }
+
+// function sum(a, b){
+//     return a + b
+// }
+
+// function calc(){
+//     var a = +prompt('Enter value 1')
+//     var b = +prompt('Enter value 2')
+//     var c = prompt('Enter operator')
+
+//     if(c == '+'){
+//         return a + b
+//     }
+//     else if(c == '-'){
+//         return a - b
+//     }
+//     else if(c == '/'){
+//         return a / b
+//     }
+//     else if(c == '*'){
+//         return a * b
+//     }
+// }
+
+function foo(){
+    // input ki value console main deni hai
+    var getInp = document.getElementById('inp')
+    console.log(getInp.value)
+}
