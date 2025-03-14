77c57ee39cdd348f4aa51a5d48c3d17fa3c95ede
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-03-14 10:20:09 +0500

    switch statement

---
+++ Javascript/class 21/app.js
@@ -0,0 +1,77 @@
+// var user = "saad"
+// switch (user) {
+//     case 'rafay':
+//         document.writeln('value is true')
+//         break;
+//     case 'saad':
+//         document.writeln('value is false')
+//         break
+// }
+
+
+// var colorName = prompt("Enter your color name")
+// switch (colorName) {
+//     case 'red':
+//         document.writeln('danger is coming soon')
+//         break
+//     case 'green':
+//         document.writeln('success is coming soon')
+//         break;
+//     case 'yellow':
+//         document.writeln('warning is coming soon')
+//         break
+//     default:
+//         document.writeln(colorName + " is not coming soon")
+// }
+
+
+// var portal = prompt("Enter your role")
+// switch (portal) {
+//     case 'admin':
+//         location.href = './admin.html'
+//         break;
+//     case 'student':
+//         location.href = './student.html'
+//         break;
+//     case 'teacher':
+//         location.href = './teacher.html'
+//         break
+// }
+
+// switch(true){
+//     case 10 > 5 || "r" === "r":
+// }
+
+
+// var colorName = prompt("Enter your color name")
+// document.body.style.backgroundColor = colorName
+
+// var colorName = prompt("Enter your color name").toLowerCase()
+// switch(colorName){
+//     case colorName:
+//         document.body.style.backgroundColor = colorName
+// }
+
+// switch (colorName) {
+//     case 'red':
+//         document.body.style.backgroundColor = 'red'
+//         break
+//     case 'green':
+//         document.body.style.backgroundColor = 'green'
+//         break
+// }
+
+
+// var user = true
+// if (!user) {
+//     document.writeln(new Date())
+// }
+// else{
+//     document.writeln("login first")
+// }
+
+
+
+
+
+
