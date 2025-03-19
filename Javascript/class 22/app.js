c7d332f581c8486692b28e3c905d39b1b445759e
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-03-19 10:15:36 +0500

    41 to 48

---
+++ Javascript/class 22/app.js
@@ -0,0 +1,31 @@
+// var i=1;
+// while( i <= 10){
+//     document.writeln(`2 x ${i} = ${2 * i} <br>`)
+//     i++
+// }
+
+/*
+var i = 21
+do {
+    document.writeln(i + "<br>")
+    i++
+}
+while (i <= 20)
+*/
+
+function chng(){
+    var getImg = document.getElementById('img')
+    getImg.src = 'https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs='
+}
+
+
+
+// function foo(e){
+//     e.style.color = 'white'
+//     e.style.backgroundColor = 'black'
+// }
+
+// function picChange(){
+//     var getPic = document.getElementById('pic')
+//     getPic.src = 'https://i.pinimg.com/736x/6a/cf/02/6acf02953d3a39fb6f595675f9a4040c.jpg'
+// }
