e16619d860a3920f6ccb5d90074cac3f2a277b0d
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-04-14 10:44:10 +0500

    The Dom

---
+++ Javascript/class 26/app.js
@@ -0,0 +1,48 @@
+// function changeBg(){
+//     var getParas = document.getElementsByTagName('p')
+//     for(var i=0; i < getParas.length; i++){
+//         getParas[i].className = 'main'
+
+//         // getParas[i].style.backgroundColor = 'black'
+//         // getParas[i].style.color = 'white'
+//     }
+// }
+
+// function changeBg(){
+//     var getDiv = document.getElementById('div')
+//     var getParas = getDiv.getElementsByTagName('p')
+//     for(var i=0; i < getParas.length; i++){
+//         getParas[i].style.backgroundColor = 'red'
+//     }
+// }
+
+// console.log(document.childNodes[1].childNodes[2].childNodes)
+// console.log(document.getElementById('third').parentNode.childNodes[2])
+
+
+// hasAttribute
+// getAttribute
+// setAttribute
+
+
+
+function check(a){
+    var getInput = document.getElementById('inp')
+    getInput.setAttribute('class', 'form-control')
+    a.setAttribute('class','btn btn-success')
+    // getInput.setAttribute('class','')
+    // console.log(getInput.getAttribute('class'))
+    // console.log(getInput.hasAttribute('class'))
+}
+
+
+
+
+
+
+
+
+
+
+
+
